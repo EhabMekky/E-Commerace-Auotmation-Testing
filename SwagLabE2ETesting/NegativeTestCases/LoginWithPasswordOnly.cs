f48671f549982301d33d3716cb9025dbbaa30ccb
Ehab Mohamed Khallaf <ehabkhallaf1@gmail.com>
2024-08-11 17:44:33 +0300

    Negative Test Case: LoginWithPasswordOnly.cs

---
+++ SwagLabE2ETesting/NegativeTestCases/LoginWithPasswordOnly.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace SwagLabE2ETesting.NegativeTestCases;
+
+public class LoginWithPasswordOnly : Setup
+{
+    [Test]
+    public void LoginWithPasswordOnlyTest()
+    {
+        // Log in
+        _driver.FindElement(By.Id("password")).SendKeys("secret_sauce");
+        _driver.FindElement(By.Id("login-button")).Click();
+        
+        // Verify error message
+        Assert.AreEqual("Epic sadface: Username is required", _driver.FindElement(By.ClassName("error-message-container")).Text);
+    }
+}
