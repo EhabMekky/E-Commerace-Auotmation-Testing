a63c22e24d60202622e5d85a4fdf9aa9af423534
Ehab Mohamed Khallaf <ehabkhallaf1@gmail.com>
2024-08-11 17:37:54 +0300

    Negative Test Case: LoginWithBlankUsernameAndPassword.cs

---
+++ SwagLabE2ETesting/NegativeTestCases/LoginWithBlankUsernameAndPassword.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace SwagLabE2ETesting.NegativeTestCases;
+
+public class LoginWithBlankUsernameAndPassword : Setup
+{
+    [Test]
+    public void LoginWithBlankUsernameAndPasswordTest()
+    {
+        // Log in
+        _driver.FindElement(By.Id("user-name")).SendKeys("");
+        _driver.FindElement(By.Id("password")).SendKeys("");
+        _driver.FindElement(By.Id("login-button")).Click();
+        
+        // Verify error message
+        Assert.AreEqual("Epic sadface: Username is required", _driver.FindElement(By.ClassName("error-message-container")).Text);
+    }
+}
